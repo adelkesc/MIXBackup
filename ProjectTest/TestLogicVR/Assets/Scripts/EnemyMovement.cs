6c56c9800d93463db3a995932c7056a8cea454f7
adelkesc <256523@via.dk>
2020-10-01 22:00:59 +0200

    Now to really put the gitignore to the test.

---
+++ ProjectTest/TestLogicVR/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private EnemyShoot enemyShoot;
+    private float rotateSpeed = 3.0f;  //2.0
+    private float rotationRange = 75.0f;
+
+    //added for the sake of testing a new movement using deltatime
+    private float frameTime;
+    private float phase;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyShoot = GameObject.FindObjectOfType<EnemyShoot>();
+        StartCoroutine(Search());
+    }
+
+    public IEnumerator Search()
+    {
+        var oscillate = StartCoroutine(Oscillate());
+        while (!enemyShoot.FoundPlayer())
+        {
+            yield return null;
+        }
+        Debug.Log("Player Found by Enemy!");
+        StopCoroutine(oscillate);
+        StartCoroutine(enemyShoot.Fire());
+    }
+
+    private IEnumerator Oscillate()
+    {
+        while (true)
+        {
+            //transform.rotation = Quaternion.Euler(0.0f, rotationRange * Mathf.Sin(Time.time * rotateSpeed), 0.0f);
+            frameTime = frameTime + Time.deltaTime;
+            phase = Mathf.Sin(frameTime / rotateSpeed);
+            transform.localRotation = Quaternion.Euler(new Vector3(0, phase * rotationRange, 0));
+            Debug.Log("Searching!");
+            yield return null;
+        }
+    }
+}
---
+++ ProjectTest/TestLogicVR/Assets/Scripts/EnemyShoot.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShoot : MonoBehaviour
+{
+    private EnemyMovement movement;
+
+    public float damage = 10.0f;
+    public float range = 100.0f;
+    RaycastHit hit;
+
+    private void Start()
+    {
+        movement = GameObject.FindObjectOfType<EnemyMovement>();
+    }
+
+    public bool FoundPlayer()
+    {
+        Debug.DrawRay(transform.position, transform.forward * -50, Color.red);
+        if (!Physics.Raycast(transform.position, transform.forward * -1, out hit, range))
+        {
+            return false;
+        }
+        return hit.transform.name == "Player";
+    }
+
+    public IEnumerator Fire()
+    {
+        yield return new WaitForSeconds(2f);
+        while (FoundPlayer())
+        {
+            Debug.Log("Is Called.");
+            PlayerHealth player = hit.transform.GetComponent<PlayerHealth>();
+            player.TakeDamage(damage);
+            yield return new WaitForSeconds(2f);
+        }
+        StartCoroutine(movement.Search());
+    }
+
+}
